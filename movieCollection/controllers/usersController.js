df35df1b789cc9ad5d850cae021aba060654ab2c
Farooq Toppa <farooqtoppa@gmail.com>
2016-10-04 14:11:21 -0400

    created new route

--- movieCollection/controllers/usersController.js
+++ movieCollection/controllers/usersController.js
@@ -7,14 +7,27 @@ var Schema = require("../db/schema.js");
 var User = Schema.User
 var Movie = Schema.Movie
 
-// =====================================================
-// USERS INDEX ROUTE (Shows all instances of a resource)
-// =====================================================
+// ==============================
+// INDEX ROUTE
+// ==============================
 router.get('/', function(req, res){
     User.find({}, function(err, users){
         res.render('users/index', {users: users});
     });
 });
+
+// ================================
+// NEW ROUTE
+// ================================
+router.get('/new', function(req, res){
+    res.render('users/new');
+});
+
+// ================================
+// CREATE ROUTE
+// ================================
+
+
 
 
 // export router
